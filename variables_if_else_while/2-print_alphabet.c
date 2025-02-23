4f2f8bfe63986aa54b4ab2f3c7f41fcb68b71d50
Bary-G <b9059289@gmail.com>
2025-02-23 21:21:13 +0000

    alph

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,18 @@
 * main(void) (- Generates alphabet)
 * return (0); (- Confirm if sucess)
 *
-* Write a program that prints the alphabet in lowercase, followed by a new line.
+* Write a program that prints the alphabet
 */
 
-int main()
+int main(void)
 {
-    char c;
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        putchar(c);
-    }
-    putchar('\n');
-    return (0);
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
