700066d394e86009ea0bbaab0a7c2cc4aa1e2966
Bary-G <b9059289@gmail.com>
2025-02-23 22:30:33 +0000

    Smile in the mirror

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,25 +2,32 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+* main(void) (- Generates numbers)
+* return (0); (- Confirm if sucess)
+*
+* Check the last digit
+*/
+
 int main(void)
 {
-	int n;
+        int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	int lastdigit = n % 10;
-	printf("Last digit of %d is %d\n", n, lastdigit)
-	if  lastdigit > 5
-	{
-		printf("and is greater than 5")
-	}
-	if lastdigit == 0
-	{
-		printf("and is 0")
-	}
-	if lastdigit < 6 && lastdigit =~ 0
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        int lastdigit = n % 10;
+        printf("Last digit of %d is %d\n", n, lastdigit)
+        if  (lastdigit > 5)
         {
-	       printf("and is less than 6 and not 0")
+                printf("and is greater than 5")
+        }
+        if (lastdigit == 0)
+        {
+                printf("and is 0")
+        }
+        if (lastdigit < 6 && lastdigit != 0)
+        {
+               printf("and is less than 6 and not 0")
         }
         return(0);
 }
