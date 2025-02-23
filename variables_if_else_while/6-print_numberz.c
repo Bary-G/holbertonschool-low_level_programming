877b1f3b9f9243f8e342fe10ca0e91cbb8e1fa7e
Bary-G <b9059289@gmail.com>
2025-02-23 21:57:51 +0000

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main(void) (- Generates numbers)
+* return (0); (- Confirm if sucess)
+*
+* Write digit Numbers base from 10
+*/
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
