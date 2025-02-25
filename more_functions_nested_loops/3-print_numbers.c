d26682514c20981721570279b439f44b7a48b833
Bary-G <b9059289@gmail.com>
2025-02-25 17:47:29 +0000

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - my function
+*
+* Return: my function
+*/
+
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
