4e7efeb1897cab0338a8e45a076f80063ae3d43f
Bary-G <b9059289@gmail.com>
2025-02-25 05:32:00 +0000

    head

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - my function
+ * @void: my value
+ *
+ * Return: my function
+ */
+
 int main(void)
 {
-        printf("_putchar\n");
+	printf("_putchar\n");
 	return (0);
 }
