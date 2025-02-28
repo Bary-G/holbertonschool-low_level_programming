60e068bef30fe888b0f20b5054639ecd8f7e6382
Bary-G <b9059289@gmail.com>
2025-02-28 13:58:26 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - my function
+ * @n: my value
+ *
+ * Return: my function
+ */
+
+int _strlen(char *s)
+{
+	int l = strlen(s);
+
+	return(l);
+}
