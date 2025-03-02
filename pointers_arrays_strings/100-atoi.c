2a8c4632db3ecf2bf2fa3483c7f2dc915eb3a08b
Bary-G <b9059289@gmail.com>
2025-03-02 11:41:00 +0000

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int _atoi(char *s)
+{
+	int num = 0;
+	int sign = 1;
+	int sign_count = 0;
+
+	if (s == NULL)
+	{
+		return 0;
+	}
+	while (isspace(*s))
+	{
+		s++;
+	}
+	while (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+		{
+			sign_count++;
+		}
+		s++;
+	}
+	if (sign_count % 2 != 0)
+	{
+		sign = -1;
+	}
+	while (*s && !isdigit(*s))
+	{
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+	{
+		num = num * 10 + (*s - '0');
+		s++;
+	}
+
+	return (sign * num);
+}
