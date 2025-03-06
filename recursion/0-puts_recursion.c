1046c9eb67baa48bdcc62e10c112441a5a501577
Bary-G <b9059289@gmail.com>
2025-03-06 14:01:11 +0000

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - my function
+ * @s: my pointer
+ *
+ * Return: my function
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
