18d118eb2d88b3f827f8b6e023cb23235c298b41
Bary-G <b9059289@gmail.com>
2025-03-11 09:18:33 +0000

    str_concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * string_length - calculate the length of a string
+ * @str: string
+ *
+ * Return: string length
+ */
+size_t string_length(char *str)
+{
+	size_t length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * str_concat - concatenate two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: concatenated strings
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t length1 = string_length(s1);
+	size_t length2 = string_length(s2);
+	char *result = (char *)malloc(length1 + length2 + 1);
+	size_t i;
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < length1; i++)
+	{
+		result[i] = s1[i];
+	}
+
+	for (i = 0; i < length2; i++)
+	{
+		result[length1 + i] = s2[i];
+	}
+
+	result[length1 + length2] = '\0';
+
+	return (result);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
 
 int _putchar(char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+size_t string_length(char* str);
+char *str_concat(char *s1, char *s2);
 
 #endif
