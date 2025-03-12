a80542158082589ba26310f334f28c47ed475c3b
Bary-G <b9059289@gmail.com>
2025-03-12 08:28:56 +0000

    alloc_grid

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -8,5 +8,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 size_t string_length(char* str);
 char *str_concat(char *s1, char *s2);
+void print_grid(int **grid, int width, int height);
+int **alloc_grid(int width, int height);
 
 #endif
