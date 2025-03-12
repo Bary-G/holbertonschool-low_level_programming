d16fc96b32ffdaf8d00bf2376b36105f75375e84
Bary-G <b9059289@gmail.com>
2025-03-12 14:18:02 +0000

    4

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -10,5 +10,6 @@ size_t string_length(char* str);
 char *str_concat(char *s1, char *s2);
 void print_grid(int **grid, int width, int height);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
