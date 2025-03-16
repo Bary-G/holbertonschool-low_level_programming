94f4416fa16ef9338459c74d7377ef0d1c32fe20
Bary-G <b9059289@gmail.com>
2025-03-16 22:05:53 +0000

    b

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -6,7 +6,8 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 int _strlen_recursion(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-void simple_print_buffer(char *buffer, unsigned int size);
 void *_calloc(unsigned int nmemb, unsigned int size);
+void simple_print_buffer(int *buffer, unsigned int size);
+int *array_range(int min, int max);
 
 #endif
