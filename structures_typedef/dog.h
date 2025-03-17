1f92140aa28e68b3b868cc4e35946661fc1517bf
Bary-G <b9059289@gmail.com>
2025-03-17 06:36:31 +0000

    doggo

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * dog - structure
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+ *
+ * Return: no
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+dog_t;
