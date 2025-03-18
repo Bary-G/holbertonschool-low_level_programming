8f195202885ea3ea926ce3fc3ac499f62262f118
Bary-G <b9059289@gmail.com>
2025-03-18 07:50:03 +0000

    Bob DYLAN

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function to print a name
+ * @name: name of the person
+ * @f: function pointer
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
