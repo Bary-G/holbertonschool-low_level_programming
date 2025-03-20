ed1e5b5ea640e5bd038724abf128fa58e36890af
Bary-G <b9059289@gmail.com>
2025-03-20 10:29:11 +0000

    op

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,4 +1,25 @@
+#include "3-calc.h"
+#include "3-main.c"
+#include "3-get_op_func.c"
+#incude <stdio.h>
+
 int op_add(int a, int b)
 {
 	return (a + b);
+}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+int op_mod(int a, int b)
+{
+	return (a % b);
 }
