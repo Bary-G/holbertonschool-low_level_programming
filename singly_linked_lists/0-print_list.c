cb41d8cd72299aff42301652fae37c4fcbc67281
Bary-G <b9059289@gmail.com>
2025-04-02 14:18:13 +0200

    task0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,22 +13,20 @@ size_t print_list(const list_t *h)
 {
 	const list_t *current = h;
 	size_t count = 0;
-	int is_null = 1;
-	char *null_str = "(null)";
 
-	if (current == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
 	while (current != NULL)
 	{
-		if (current == "(null)")
+		if (current->str == NULL)
 		{
-			printf("[%ld] %s\n", count, "(nil)");
+			printf("[0] (nil)\n");
 		}
-		printf("[%ld] %s\n", count, current->str);
+		else
+		{
+			printf("[%ld] %s\n", count, current->str);
+		}
 		current = current->next;
 		count++;
 	}
+
 	return (count);
 }
