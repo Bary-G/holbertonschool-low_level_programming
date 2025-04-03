5d65118e3a85e7c03c6798fc00b8a0b1d20c674f
Bary-G <b9059289@gmail.com>
2025-04-03 14:57:15 +0200

    task4

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,20 +12,35 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newNode = malloc(sizeof(list_t));
-	result_t result = {NULL, *head};
+	list_t *newNode;
+	unsigned int len = 0;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len])
+	{
+		len++;
+	}
+	newNode = malloc(sizeof(list_t));
 	if (newNode == NULL)
 	{
-        return (NULL);
-    }
+		return (NULL);
+	}
 	newNode->str = strdup(str);
+	newNode->len = len;
+	newNode->next = NULL;
 	if (newNode->str == NULL)
 	{
 		free(newNode);
-		return (result);
+		return (NULL);
 	}
-	newNode->next = *head;
+	while (current->next)
+	{
+		current = current->next;
+	}
+	current->next = newNode;
 	*head = newNode;
 
 	return (newNode);
