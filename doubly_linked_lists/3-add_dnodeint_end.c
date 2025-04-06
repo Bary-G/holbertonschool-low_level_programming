c2de30fa8da3a0dbfd0b20cba301faa4b2a7cefe
Bary-G <b9059289@gmail.com>
2025-04-06 08:46:19 +0200

    task3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,6 @@
  *
  * Return: the new node
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node, *temp;
@@ -21,7 +20,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	if (*head == NULL)
-    {
+	{
 		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
