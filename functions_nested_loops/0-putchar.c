f1b8d106452f7be8f7f98bc2ec3915ae4d95667b
Bary-G <b9059289@gmail.com>
2025-05-04 21:55:02 +0200

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,11 +5,17 @@
  * main - my function
  * @void: my value
  *
- * Return: my function
+ * Return: 0
  */
-
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+    char text[] = "_putchar\n";
+    int i;
+
+    for (i = 0; text[i] != '\0'; i++)
+    {
+        _putchar(text[i]);
+    }
+
+    return (0);
 }
