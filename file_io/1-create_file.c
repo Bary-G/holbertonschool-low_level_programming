b3793c0ef417f7c61529ec1f3f981c5cc38f1a81
Bary-G <b9059289@gmail.com>
2025-05-04 08:58:14 +0200

    t1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,34 +1,29 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-/**
- * create_file - create a file
- *
- * @filename: filename
- * @text_content: file content to write
- *
- * Return: 0 if sucess, -1 if not
- */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes_written;
-	size_t len = 0;
 
 	asm volatile (
 		"mov $2, %%eax\n\t"
 		"mov %1, %%rdi\n\t"
-		"mov $0101, %%esi\n\t"
+		"mov $01101, %%esi\n\t"
 		"mov $0600, %%edx\n\t"
 		"syscall"
 		: "=a" (fd)
 		: "r" (filename)
 		: "rdi", "rsi", "rdx"
 	);
+
 	if (fd < 0)
+	{
 		return (-1);
+	}
 	if (text_content)
 	{
+		size_t len = 0;
 		while (text_content[len]) len++;
 		{
 			asm volatile (
@@ -55,5 +50,6 @@ int create_file(const char *filename, char *text_content)
 		: "r" (fd)
 		: "edi"
 	);
+
 	return (0);
 }
