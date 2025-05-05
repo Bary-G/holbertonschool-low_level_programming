c27632a909fe68a40b0fe5dd5da31b42dcf1209f
Bary-G <b9059289@gmail.com>
2025-05-05 14:31:48 +0200

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,6 +4,45 @@
 #include "dog.h"
 
 /**
+ * _strlen - my function
+ * @s: my value
+ *
+ * Return: my function
+ */
+
+ int _strlen(char *s)
+ {
+	 int l = 0;
+
+	 while (s[l] != '\0')
+	 {
+		 l++;
+	 }
+	 return (l);
+ }
+
+ /**
+ * _strcpy - my function
+ * @dest: my pointer
+ * @src: my value
+ *
+ * Return: my function
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*src)
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog struct.
  * @name: new dog name
  * @age: new dog age
@@ -15,24 +54,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
+	{
 		return (NULL);
-
-	dog->name = malloc(strlen(name) + 1);
+	}
+	dog->name = malloc(_strlen(name) + 1);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	strcpy(dog->name, name);
+	_strcpy(dog->name, name);
 
-	dog->owner = malloc(strlen(owner) + 1);
+	dog->owner = malloc(_strlen(owner) + 1);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	strcpy(dog->owner, owner);
+	_strcpy(dog->owner, owner);
 
 	dog->age = age;
 	return (dog);
