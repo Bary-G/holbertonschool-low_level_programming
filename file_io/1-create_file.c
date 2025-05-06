3503411f9ece6306da6828ca3be2a1319b28fc32
Bary-G <b9059289@gmail.com>
2025-05-06 13:26:06 +0200

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,30 +12,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t bytes_written;
-    size_t len = 0;
+	int fd;
+	ssize_t bytes_written;
+	size_t len = 0;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content)
-    {
-        while (text_content[len])
-            len++;
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
 
-        bytes_written = write(fd, text_content, len);
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
