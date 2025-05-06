8361583eca5c8f18e5bb5ea3d1ac53b3deb15326
Bary-G <b9059289@gmail.com>
2025-05-06 15:06:50 +0200

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -32,31 +32,19 @@ int _strlen(char *s)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
+	int file_descriptor = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0644);
+	ssize_t bytes_written;
 
-	if (!filename)
+	if (file_descriptor == -1)
 	{
 		return (-1);
 	}
-
-	file = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
-
-	if (file < 0)
+	bytes_written = write(file_descriptor, text_content, strlen(text_content));
+	if (bytes_written == -1)
 	{
+		close(file_descriptor);
 		return (-1);
 	}
-
-	if (text_content)
-	{
-		ssize_t bytes_written = write(file, text_content, sizeof(char) * _strlen(text_content));
-
-		if (bytes_written < 0)
-		{
-			close(file);
-			return (-1);
-		}
-	}
-
-	close(file);
+	close(file_descriptor);
 	return (1);
 }
--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -40,7 +40,6 @@ int main(int argc, char *argv[])
 		print_error_and_exit("Error: Can't read from file", argv[1], 98);
 	}
 
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
