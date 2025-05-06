6aa14b20240c265d39c25fc5b416fc60f953140d
Bary-G <b9059289@gmail.com>
2025-05-06 15:39:35 +0200

    5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,7 +9,8 @@
  *
  * Return: nothing
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index) {
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
 	unsigned int current_index = 0;
 	dlistint_t *current_node = head;
 
